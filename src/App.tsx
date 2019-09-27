18617533c649ef99bec71ba77955ca3b255f737e
leedeokmu <freeefly@bluehole.net>
2019-09-27 16:36:50 +0900

    feat : add react redux connect

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,55 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {addAge, subAge} from "./index";
+import {connect} from "react-redux"
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface AppProps {
+    age: number;
+    onAddClick(): void;
+    onSubClick(): void;
 }
 
-export default App;
+class App extends Component<AppProps, {}> {
+
+    render() {
+        const {age, onAddClick, onSubClick} = this.props;
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <p>
+                        Edit <code>src/App.tsx</code> and save to reload.
+                    </p>
+                    <a
+                        className="App-link"
+                        href="https://reactjs.org"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >Learn</a>
+                    <p>{age}</p>
+                    <button onClick={onAddClick}>an year passed...</button>
+                    <button onClick={onSubClick}>last year...</button>
+                </header>
+            </div>
+        );
+    }
+}
+
+const mapStateToProps = (state: {age: number}) => {
+    return {
+        age: state.age
+    }
+}
+
+const mapDispatchToProps = (dispatch: Function) => {
+    return {
+        onAddClick: () => dispatch(addAge()),
+        onSubClick: () => dispatch(subAge())
+    }
+}
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(App);
--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,41 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import {createStore} from "redux";
+import {Provider} from "react-redux";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const ADD_AGE = 'ADD_AGE';
+const SUB_AGE = 'SUB_AGE';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+export const addAge = (): { type: string } => {
+    return {
+        type: ADD_AGE
+    }
+};
+export const subAge = (): { type: string } => {
+    return {
+        type: SUB_AGE
+    }
+};
+
+const ageApp = (state: { age: number } = {age: 20}, action: { type: string }) => {
+    switch (action.type) {
+        case ADD_AGE:
+            return {
+                age: state.age + 1
+            };
+        case SUB_AGE:
+            return {
+                age: state.age - 1
+            };
+        default:
+            return state;
+    }
+};
+
+const store = createStore(ageApp);
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+    , document.getElementById('root'));
